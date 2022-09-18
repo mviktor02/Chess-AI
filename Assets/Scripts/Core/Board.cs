dc7924876a29af421ccc3b36ad1017a5a8565b23
mviktor02 <mviktor2210@gmail.com>
2022-09-18 20:52:10 +0200

    Loading positions, edited license, updated unity editor

--- Assets/Scripts/Core/Board.cs
+++ Assets/Scripts/Core/Board.cs
@@ -1,4 +1,6 @@
-﻿namespace Chess.Core
+﻿using System.Collections.Generic;
+
+namespace Chess.Core
 {
     // TODO
     public class Board
@@ -16,6 +18,15 @@
 
         public int plyCounter;
         public int fiftyMoveCounter;
+
+        /// <summary>
+        /// Bits 0-3 store white and black king/queenside castling legality
+        ///      4-7 store the file of the en passant square (1-8), 0 means there's no en passant square
+        ///      8-13 store the last captured piece
+        ///      14+ store the fifty move counter
+        /// </summary>
+        public uint currentGameState;
+        private Stack<uint> gameStateHistory;
         
         public int[] kingSquareIndexes;
 
@@ -47,6 +58,7 @@
             squares = new int[64];
             kingSquareIndexes = new int[2];
 
+            gameStateHistory = new Stack<uint>();
             plyCounter = 0;
             fiftyMoveCounter = 0;
 
@@ -84,7 +96,49 @@
         public void LoadPosition(string fen)
         {
             InitializeBoard();
-            // TODO
+            var loadedPosition = FenUtility.PositionFromFen(fen);
+
+            for (var index = 0; index < 64; index++)
+            {
+                var piece = loadedPosition.squares[index];
+
+                if (piece == Piece.None) continue;
+                
+                var type = Piece.GetPieceType(piece);
+                var colourIndex = Piece.IsColour(piece, Piece.White) ? WhiteIndex : BlackIndex;
+                if (Piece.IsSlidingPiece(piece))
+                {
+                    switch (type)
+                    {
+                        case Piece.Queen: queens[colourIndex].AddPieceAtSquare(index); break;
+                        case Piece.Rook: rooks[colourIndex].AddPieceAtSquare(index); break;
+                        case Piece.Bishop: bishops[colourIndex].AddPieceAtSquare(index); break;
+                    }
+                }
+                else switch (type)
+                {
+                    case Piece.Knight: knights[colourIndex].AddPieceAtSquare(index); break;
+                    case Piece.Pawn: pawns[colourIndex].AddPieceAtSquare(index); break;
+                    case Piece.King: kingSquareIndexes[colourIndex] = index; break;
+                }
+            }
+
+            isWhitesTurn = loadedPosition.whiteToMove;
+            colourToMove = isWhitesTurn ? Piece.White : Piece.Black;
+            opponentColour = isWhitesTurn ? Piece.Black : Piece.White;
+            colourToMoveIndex = isWhitesTurn ? WhiteIndex : BlackIndex;
+            
+            var whiteCastle = (loadedPosition.whiteCastleKingside  ? 1 << 0 : 0) | 
+                                 (loadedPosition.whiteCastleQueenside ? 1 << 1 : 0);
+            var blackCastle = (loadedPosition.blackCastleKingside  ? 1 << 2 : 0) | 
+                                 (loadedPosition.blackCastleQueenside ? 1 << 3 : 0);
+            var epState = loadedPosition.enPassantFile << 4;
+            var initialGameState = (ushort) (whiteCastle | blackCastle | epState);
+            gameStateHistory.Push(initialGameState);
+            currentGameState = initialGameState;
+            plyCounter = loadedPosition.plyCount;
+
+            plyCounter = loadedPosition.plyCount;
         }
 
     }
--- Assets/Scripts/Core/FenUtility.cs
+++ Assets/Scripts/Core/FenUtility.cs
@@ -1,4 +1,7 @@
-﻿namespace Chess.Core
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chess.Core
 {
     /// <summary>
     /// The easiest way to save and load gamestates in chess is to use the <a href="https://www.chessprogramming.org/Forsyth-Edwards_Notation">Forsyth-Edwards Notation</a>
@@ -7,8 +10,76 @@
     /// </summary>
     public class FenUtility
     {
-        public const string StartFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        private static Dictionary<char, int> pieceTypeFromSymbol = new()
+        {
+            ['p'] = Piece.Pawn, ['r'] = Piece.Rook, ['n'] = Piece.Knight, ['b'] = Piece.Bishop, ['q'] = Piece.Queen, ['k'] = Piece.King
+        };
         
-        // TODO load and store position
+        public const string StartFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+        public static LoadedPositionInfo PositionFromFen(string fen)
+        {
+            var loadedPositionInfo = new LoadedPositionInfo();
+            var sections = fen.Split(' ');
+
+            var file = 0;
+            var rank = 7;
+
+            foreach (var symbol in sections[0])
+            {
+                if (symbol == '/')
+                {
+                    file = 0;
+                    rank--;
+                }
+                else
+                {
+                    if (char.IsDigit(symbol))
+                    {
+                        file += (int)char.GetNumericValue(symbol);
+                    }
+                    else
+                    {
+                        var colour = char.IsUpper(symbol) ? Piece.White : Piece.Black;
+                        var type = pieceTypeFromSymbol[char.ToLower(symbol)];
+                        loadedPositionInfo.squares[rank * 8 + file] = type | colour;
+                        file++;
+                    }
+                }
+            }
+
+            loadedPositionInfo.whiteToMove = sections[1] == "w";
+
+            var castlingRights = sections[2];
+            loadedPositionInfo.whiteCastleKingside = castlingRights.Contains("K");
+            loadedPositionInfo.whiteCastleQueenside = castlingRights.Contains("Q");
+            loadedPositionInfo.blackCastleKingside = castlingRights.Contains("k");
+            loadedPositionInfo.blackCastleQueenside = castlingRights.Contains("q");
+
+            var enPassantFileName = sections[3][0].ToString();
+            if (BoardRepresentation.FileNames.Contains(enPassantFileName))
+                loadedPositionInfo.enPassantFile = BoardRepresentation.FileNames.IndexOf(enPassantFileName, StringComparison.Ordinal) + 1;
+
+            int.TryParse(sections[4], out loadedPositionInfo.plyCount);
+
+            return loadedPositionInfo;
+        }
+        
+        public class LoadedPositionInfo
+        {
+            public int[] squares;
+            public bool whiteCastleKingside;
+            public bool whiteCastleQueenside;
+            public bool blackCastleKingside;
+            public bool blackCastleQueenside;
+            public int enPassantFile;
+            public bool whiteToMove;
+            public int plyCount;
+
+            public LoadedPositionInfo()
+            {
+                squares = new int[64];
+            }
+        }
     }
 }
