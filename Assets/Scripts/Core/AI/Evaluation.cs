859daaa18d6e8c60100b35f483fb49e34c426e45
mviktor02 <mviktor2210@gmail.com>
2022-11-19 12:52:58 +0100

    material only evaluation

---
+++ Assets/Scripts/Core/AI/Evaluation.cs
@@ -0,0 +1,38 @@
+﻿namespace Chess.Core.AI
+{
+    public class Evaluation
+    {
+        
+        public const int pawnValue = 100;
+        public const int knightValue = 300;
+        public const int bishopValue = 320;
+        public const int rookValue = 500;
+        public const int queenValue = 900;
+
+        public static int EvaluateMaterialOnly(in Board board)
+        {
+            int whiteEval = 0;
+            int blackEval = 0;
+
+            int whiteMaterialScore = CalculateMaterialScore(board, Board.WhiteIndex);
+            int blackMaterialScore = CalculateMaterialScore(board, Board.BlackIndex);
+
+            whiteEval += whiteMaterialScore;
+            blackEval += blackMaterialScore;
+
+            int eval = whiteEval - blackEval;
+
+            return board.isWhitesTurn ? eval : -eval;
+        }
+
+        private static int CalculateMaterialScore(in Board board, int colourIndex)
+        {
+            return board.pawns[colourIndex].Count * pawnValue
+                   + board.knights[colourIndex].Count * knightValue
+                   + board.bishops[colourIndex].Count * bishopValue
+                   + board.rooks[colourIndex].Count * rookValue
+                   + board.queens[colourIndex].Count * queenValue;
+        }
+        
+    }
+}
